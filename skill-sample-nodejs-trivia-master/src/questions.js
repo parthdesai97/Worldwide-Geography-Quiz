0eb5c044ed7399d3e474f9d3ccf018e946edf9d3
parthdesai97 <parthdesai@Parths-MBP.attlocal.net>
2017-08-14 22:17:14 -0700

    First

---
+++ skill-sample-nodejs-trivia-master/src/questions.js
@@ -0,0 +1,351 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "Which of the following US states is the largest by area?": [
+                "New Mexico",
+                "Nevada",
+                "Oregon",
+                "Arizona"
+            ]
+        },
+        {
+            "In which US state is the Tongass National Forest?": [
+                "Alaska",
+                "Arizona",
+                "California",
+                "Arkansas",
+                "Tennessee"
+            ]
+        },
+        {
+            "With an elevation of 14,505 feet, Mount Whitney is the highestpoint in which US State?": [
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware"
+            ]
+        },
+        {
+            "The historic Jackson Hole lies in which National Park?": [
+                "Grand Teton",
+                "Great Basin",
+                "Yosemite",
+                "Everglades"
+            ]
+        },
+        {
+            "Which American Great Lake has a name that means Great Water?": [
+                "Lake Michigan",
+                "Ontario",
+                "Erie",
+                "Superior"
+            ]
+        },
+        {
+            "Which U.S. State is known as the Nutmeg State?": [
+                "Connecticut",
+                "Texas",
+                "Arizona",
+                "Delaware"
+            ]
+        },
+        {
+            "Yellowstone National Park stretches across how many US States?": [
+                "Three",
+                "Two",
+                "Five",
+                "Four"
+            ]
+        },
+        {
+            "Which U.S. states flag consists of a red X on a white background?": [
+                "Alabama",
+                "Alaska",
+                "Arkansas",
+                "Arizona"
+            ]
+        },
+        {
+            "Which of these states is the most easterly?": [
+                "Ohio",
+                "Kansas",
+                "Wyoming",
+                "Texas"
+            ]
+        },
+        {
+            "How many U.S. states begin with the letter W?": [
+                "Four",
+                "One",
+                "Three",
+                "Two"
+            ]
+        },
+        {
+            "Which of these nations does not border Lake Victoria?": [
+                "Angola",
+                "Uganda",
+                "Kanye",
+                "Tanzania"
+            ]
+        },
+        {
+            "Addis Ababa is the capital of what African country?": [
+                "Ethiopia",
+                "Kenya",
+                "Chad",
+                "Togo"
+            ]
+        },
+        {
+            "By volume, what is the largest fresh water lake in the world?": [
+                "Lake Baikal",
+                "Lake Michigan",
+                "Lake Superior",
+                "Lake Tahoe"
+            ]
+        },
+        {
+            "What is the name of the highest mountain in Africa?": [
+                "Mount Kibo",
+                "Mount Kilimanjaro ",
+                "Mount Emin",
+                "Mount Meru"
+            ]
+        },
+        {
+            "Located within the West Indies, which of these islands is not a part of the Windward Islands?": [
+                "Antigua",
+                "Grenada",
+                "Dominica",
+                "Martinique"
+            ]
+        },
+        {
+            "Located through regions of the Asian continent, whcih of these cities is located along the famous Silk Road?": [
+                "Samarkand",
+                "Tokyo",
+                "New Dehli",
+                "Bangkok"
+            ]
+        },
+        {
+            "The capital of Costa Rica, San Jose is known for producing which commodity?": [
+                "Coffee",
+                "Rice",
+                "Bananas",
+                "Pineapples"
+            ]
+        },
+        {
+            "Located in West Africa, what is the national language of Sierra Leone?": [
+                "English",
+                "German",
+                "French",
+                "Italian"
+            ]
+        },
+        {
+            "At a population of approxmately 36.5 million, which state is the most populous in the United States?": [
+                "California",
+                "Florida",
+                "New York",
+                "Texas"
+            ]
+        },
+        {
+            "What is the only multonationla continent that has no landlocked nations?": [
+                "North America",
+                "Europe",
+                "Africa",
+                "South America"
+            ]
+        },
+        {
+            "Of the following countries located in Africa, which one is the largest by area?": [
+                "Algeria",
+                "Chad",
+                "Libya",
+                "South Africa"
+            ]
+        },
+        {
+            "At a height of 7,310 feet above sea level, what is the highest mountain on the continent of Australia?": [
+                "Mount Kosciuszko",
+                "Mount Twynam",
+                "Mount Townsend",
+                "South Rams Head"
+            ]
+        },
+        {
+            "Which of the following continents does not have a major desert?": [
+                "Europe",
+                "Asia",
+                "North America",
+                "South America"
+            ]
+        },
+        {
+            "Which of the following U.S. cities has the nickname Iron City?": [
+                "Pittsburgh",
+                "Chicago",
+                "Detroit",
+                "San Francisco"
+            ]
+        },
+        {
+            "What city would you metaphorically leave your heart in if you visited it?": [
+                "San Francisco",
+                "Seattle",
+                "Pittsburgh",
+                "Portland"
+            ]
+        },
+        {
+            "With a population of just over 56,000 people, which country is the least densely populated country in the world?": [
+                "Greenland",
+                "Iceland",
+                "Canada",
+                "Finland"
+            ]
+        },
+        {
+            "Which country\'s flag is the only one to have a bible pictured on it?": [
+                "Dominican Republic",
+                "Puerto Rico",
+                "Brazil",
+                "Haiti"
+            ]
+        },
+        {
+            "Approxmately, how far away from the North Pole is North Magnetic Pole?": [
+                "Eight hundred miles",
+                "Four hundred miles",
+                "Two hundred miles",
+                "Six hundred miles"
+            ]
+        },
+        {
+            "Of the following countries located in Europe, which one is the smallest?": [
+                "Italy",
+                "Spain",
+                "Poland",
+                "Sweden"
+            ]
+        },
+        {
+            "which city is home to the oldest university in California, University of the Pacific?": [
+                "Stockton",
+                "Fresno",
+                "San Francisco",
+                "Los Angeles"
+            ]
+        },
+        {
+            "Which state is home to Elvis Presley\'s home, Graceland?": [
+                "Tennessee",
+                "Louisiana",
+                "Georgia",
+                "California"
+            ]
+        },
+        {
+            "Which California county is home to Muir Woods National Forest?": [
+                "Marin County",
+                "Orange County",
+                "Los Angeles County",
+                "Humboldt County"
+            ]
+        },
+        {
+            "What razor-thin country accounts for more than half of the western coastline of South America?": [
+                "Chile",
+                "Ecuador",
+                "Bolivia",
+                "Portugal"
+            ]
+        },
+        {
+            "What is the only country to have a non-quadrilateral flag?": [
+                "Nepal",
+                "Tibet",
+                "China",
+                "Switzerland"
+            ]
+        },
+        {
+            "What is the name of the river that runs through Baghdad?": [
+                "Tigris River",
+                "Nile River",
+                "Karun River",
+                "Ganges River"
+            ]
+        },
+        {
+            "What country has the most natural lakes?": [
+                "Canada",
+                "United States",
+                "Australia",
+                "Russia"
+            ]
+        },
+        {
+            "What is the driest place on Earth?": [
+                "McMurdo Dry Valleys, Antartica",
+                "Atacama Desert, Peru",
+                "Sahara Desert, Africa",
+                "Death Valley, California"
+            ]
+        },
+        {
+            "In what country can you visit Machu Picchu": [
+                "Peru",
+                "Chile",
+                "Colombia",
+                "Bolivia"
+            ]
+        },
+        {
+            "What is the oldest city in the world?": [
+                "Damascus, Syria",
+                "Athens, Greece",
+                "Jerusalem, Israel",
+                "Alexandria, Egypt"
+            ]
+        },
+        {
+            "Which U.S. state has the most active volcanoes?": [
+                "Alaska",
+                "Hawaii",
+                "Washington",
+                "California"
+            ]
+        },
+        {
+            "What is the flattest continent?": [
+                "Australia",
+                "Antartica",
+                "South America",
+                "Africa"
+            ]
+        },
+        {
+            "What country has the most coastline in miles?": [
+                "Canada",
+                "China",
+                "Russia",
+                "Greenland"
+            ]
+        },
+
+
+
+    ]
+};
+
